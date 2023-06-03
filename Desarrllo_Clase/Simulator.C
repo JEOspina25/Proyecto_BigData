d36dbbc147cb187af77b397135c4a58bf6a064eb
JFCastello <juan.castello@udea.edu.co>
2023-06-03 17:02:19 -0500

    Avance 1 Clase

---
+++ Desarrllo_Clase/Simulator.C
@@ -0,0 +1,51 @@
+#include <TROOT.h>
+#include "TMath.h"
+#include <iostream>
+#include <fstream>
+#include "RooFit.h"
+#include "RooFitResult.h"
+#include "RooGlobalFunc.h"
+#include "RooArgusBG.h"
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooGaussian.h"
+#include "RooCBShape.h"
+#include "RooExponential.h"
+#include "RooGenericPdf.h"
+#include "RooFFTConvPdf.h"
+#include "RooPolynomial.h"
+#include "RooChebychev.h"
+#include "RooAddPdf.h"
+#include "TPaveText.h"
+#include "TLegend.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TGaxis.h"
+#include "TAxis.h"
+#include "TLatex.h"
+#include "RooPlot.h"
+#include "RooHist.h"
+#include "RooNumIntConfig.h"
+
+using namespace RooFit;
+using namespace std;
+
+class Simulator{
+    /*Esta clase recibe como input un modelo pdf con sus respectivos argumentos y genera a partir de ello
+    1.) datos sinteticos
+    2.) un fit a esos datos sinteticos
+    3.) un pull y montecarlo de dichos datos
+    
+    todo esto solo con findes educativos de formacion*/
+    private:
+
+    public:
+
+
+};
+
+
+
+
+
